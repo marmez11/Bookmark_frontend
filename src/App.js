1099bffbf81c68d7c356eca6ac5bc98fd565e934
Nate Stodd <nathan.stoddard@gmail.com>
2023-01-17 20:06:29 -0500

    updated netlify file & text on app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Group 10 is the Best!
         </p>
         <a
           className="App-link"
